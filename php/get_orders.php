79a2b377f2383e43e788b3eb4accdd1be83b292e
Gnaniasam <gnaniasam@gmail.com>
2025-09-17 20:55:49 +0530

    Delete php directory

--- php/get_orders.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-$result = $conn->query("SELECT * FROM orders ORDER BY id DESC");
-$orders = [];
-while($row = $result->fetch_assoc()){
-    $orders[] = $row;
-}
-echo json_encode($orders);
-?>
--- php/get_products.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-$result = $conn->query("SELECT * FROM products ORDER BY id DESC");
-$products = [];
-while($row = $result->fetch_assoc()){
-    $products[] = $row;
-}
-echo json_encode($products);
-?>
